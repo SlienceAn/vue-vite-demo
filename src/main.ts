f72701da077827287469f36a505f79966d732566
tingan <tingan@ch-si.com.tw>
2024-02-20 16:42:10 +0800

    測試

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ app.component('LMap', LMap)
 app.component('LTileLayer', LTileLayer)
 app.component('LMarker', LMarker)
 app.component('CircleLoading', Loading)
-
+console.log('test')
 for (const icons in materialIcons) {
   app.component(`mti-${icons}`, materialIcons[icons])
 }
