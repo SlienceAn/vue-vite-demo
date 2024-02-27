44ae21e2fa5010a756ea588a2af17d7d2bcd5e8a
tingan <tingan@ch-si.com.tw>
2024-02-27 15:54:33 +0800

    修改Build配置

--- vite.config.ts
+++ vite.config.ts
@@ -22,11 +22,30 @@ export default defineConfig((config) => {
       assetsDir:'static/img/',
       rollupOptions:{
         output:{
-          chunkFileNames:'static/js/chunk/[name]-[hash].js',
-          entryFileNames:'static/js/entry/[name]-[hash].js',
-          assetFileNames:'static/[ext]/[name]-[hash].[ext]'
+          chunkFileNames:'static/js/chunk/[name].js',
+          entryFileNames:'static/js/entry/[name].js',
+          assetFileNames:'static/[ext]/[name].[ext]',
+          //手動拆分node_modules檔案
+          manualChunks(id) {
+            if(id.includes('node_modules')) {
+              const arr = id.toString().split('node_modules/.pnpm/')[1].split('/')[0].replace(/^@/, '').replace(/\+/g, '_')
+              const modules = arr.split('@')[0]
+              switch(modules) {
+              case 'axios':
+              case 'element-plus':
+              case 'dayjs':
+              case 'lodash-es':
+              case 'ctrl_tinycolor':
+                return modules
+              default:
+                return 'vendor'
+              }
+            }
+          }
         }
-      }
+      },
+      minify:'esbuild',
+      target:'esnext'
     },
     plugins: [
       vue(),
