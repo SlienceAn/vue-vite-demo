bf87b71a3ba3bbcee21a6ff5d273a470f0f32cb2
tingan <tingan@ch-si.com.tw>
2024-12-12 17:20:36 +0800

    element plus新增locale

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
-  <el-config-provider :button="config">
+  <el-config-provider
+    :button="config"
+    :locale="zhCn"
+  >
     <router-view />
   </el-config-provider>
 </template>
 <script setup lang="ts">
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 // import { useWindowSize } from '@vueuse/core'
 const config = reactive({
   autoInsertSpace: true,
