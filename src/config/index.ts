4729b3870a2b729f6551ef1628a49321befb0cff
tingan <tingan@ch-si.com.tw>
2024-12-20 17:29:38 +0800

    調整pusher

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,7 @@
 export default {
-  name:'AIOT(demo)'
+  name: 'AIOT(demo)',
+  key: import.meta.env.VITE_PUSHER_KEY,
+  cluster: import.meta.env.VITE_PUSHER_CLUSTER,
+  PUSHER_SUCCESS: 'pusher:subscription_succeeded',
+  PUSER_ERROR: 'pusher:subscription_error'
 }
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,19 +1,15 @@
 import Pusher, { Channel } from 'pusher-js'
-import { ElNotification } from 'element-plus'
-
-const key = import.meta.env.VITE_PUSHER_KEY
-const cluster = import.meta.env.VITE_PUSHER_CLUSTER
+import { ElNotification, dayjs } from 'element-plus'
+import config from '@/config'
+const { key, cluster, PUSER_ERROR, PUSHER_SUCCESS } = config
 const pusher = new Pusher(key, { cluster })
-const PUSHER_SUCCESS = 'pusher:subscription_succeeded'
-const PUSER_ERROR = 'pusher:subscription_error'
 
 let settingChannel: Channel | null = null
 let notificationChannel: Channel | null = null
-
+// 缺驗證
 const createPuser = () => {
   const init = () => {
     const userStore = useUserForm()
-    const notificationStore = useNotification()
 
     // 先清理可能存在的舊訂閱
     if (settingChannel) {
@@ -21,9 +17,14 @@ const createPuser = () => {
       pusher.unsubscribe('Setting')
     }
 
+    if (notificationChannel) {
+      notificationChannel.unbind_all()
+      pusher.unsubscribe('notification')
+    }
+
     // 等待連接成功後再訂閱
     pusher.connection.bind('connected', () => {
-
+      userStore.$patch({ isConnect: true })
       settingChannel = pusher.subscribe('setting')
       notificationChannel = pusher.subscribe('notification') // 最新消息頻道
 
@@ -31,7 +32,6 @@ const createPuser = () => {
       settingChannel
         .bind(PUSHER_SUCCESS, () => {
           userStore.getAll()
-          userStore.$patch({ isConnect: true })
         })
         .bind(PUSER_ERROR, (error) => {
           console.log('pusher error', error)
@@ -43,18 +43,17 @@ const createPuser = () => {
 
       // 最新消息頻道
       notificationChannel
-        .bind(PUSHER_SUCCESS,()=>{
+        .bind(PUSHER_SUCCESS, () => {
           console.log('通知訂閱成功')
         })
-        .bind(PUSER_ERROR,()=>{
+        .bind(PUSER_ERROR, () => {
           console.log('通知訂閱失敗')
         })
         .bind('update', (chunk) => {
-          notificationStore.addList(chunk)
           ElNotification({
             type: chunk.type,
-            title:chunk.title,
-            message: chunk.message
+            title: chunk.title,
+            message: `${chunk.message} ${dayjs(chunk.date).format('YYYY-MM-DD HH:mm')}`
           })
         })
     })
