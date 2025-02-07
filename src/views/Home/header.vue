df00f1c14b197a14507482e2f0fc45b46c6e0a17
tingan <tingan@ch-si.com.tw>
2025-02-07 11:53:47 +0800

    新增行動版下拉選單

--- src/views/Home/header.vue
+++ src/views/Home/header.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="icons hidden-xs-only">
+  <span class="icons">
     <el-icon size="24">
       <ElementPlus />
     </el-icon>
@@ -17,6 +17,31 @@
       {{ R.meta.title }}
     </el-breadcrumb-item>
   </el-breadcrumb>
+  <el-dropdown
+    trigger="click"
+    class="inline-flex hidden-md-and-up ml-auto"
+  >
+    <span class="dropdown">
+      選 單
+      <el-icon class="el-icon--right">
+        <arrow-down />
+      </el-icon>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item
+          v-for="R in data.menu"
+          :key="R.path"
+        >
+          <router-link
+            :to="{ path: R.path }"
+          >
+            {{ R.meta.title }}
+          </router-link>
+        </el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
 </template>
 <script lang="ts" setup>
 import config from '@/config'
@@ -24,14 +49,26 @@ const loginStore = useLoginStore()
 const { data } = storeToRefs(loginStore)
 </script>
 <style lang="scss" scoped>
-.icons{
+.icons {
   @apply font-700 inline-flex items-center gap-1;
 }
-.bread{
-  @apply inline-flex items-center ml-auto ;
+
+.bread {
+  @apply inline-flex items-center ml-auto;
 }
-:deep(
-.el-breadcrumb__inner.is-link) {
-  @apply font-700 text-[var(--el-text-color-primary)] ;
+
+:deep(.el-breadcrumb__inner.is-link) {
+  @apply font-700 text-[var(--el-text-color-primary)];
+}
+
+.dropdown {
+  @apply el-dropdown-link un-border inline-flex items-center font-bold py-2 px-4 rounded-lg;
+}
+:deep(.el-dropdown-menu__item ){
+  @apply p-0 ;
+
+  & a {
+    @apply no-underline block w-full text-center text-black font-700 py-[5px] px-[16px];
+  }
 }
 </style>
